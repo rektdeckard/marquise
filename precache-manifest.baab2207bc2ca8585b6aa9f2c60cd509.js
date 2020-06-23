f7db7fc9398f6e1d8afeb27c8fdfdbf74f5aea9f
rektdeckard <friedtm@gmail.com>
2020-06-23 03:34:45 -0400

    Updates

---
+++ precache-manifest.baab2207bc2ca8585b6aa9f2c60cd509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b61cd7701c51b8a630a8012f21f01e3",
+    "url": "/github.io/marquise/index.html"
+  },
+  {
+    "revision": "204022816a70d8522101",
+    "url": "/github.io/marquise/static/css/main.7803f931.chunk.css"
+  },
+  {
+    "revision": "2e16b91aad962411c1df",
+    "url": "/github.io/marquise/static/js/2.1769f16e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/marquise/static/js/2.1769f16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "204022816a70d8522101",
+    "url": "/github.io/marquise/static/js/main.911093a8.chunk.js"
+  },
+  {
+    "revision": "54db564f6d9f8df6e2f8",
+    "url": "/github.io/marquise/static/js/runtime-main.473cd055.js"
+  }
+]);
